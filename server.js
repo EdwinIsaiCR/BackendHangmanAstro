e4b2d2375f40f5af48ef913a1b9dc27ff0b89cd8
Edwin <bomboncinedwin@gmail.com>
2025-05-12 16:01:12 +0200

    cors1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://hangman-astro.vercel.app', 'http://localhost:3000']
+  origin: ['https://hangman-astro.vercel.app', 'http://localhost:4321']
 }));
 app.use(express.json()); // Para parsear JSON en el body
 
